cae5aa0d4242ea786d303d08549788284bf0bfdc
dgwedo <duongvu98832@gmail.com>
2020-08-13 11:55:40 +0700

    update

--- admin.js
+++ admin.js
@@ -1,4 +1,50 @@
 $(document).ready(function () {
+    // option = $("#muc").val()
+    $("form").submit(function () {
+        option = $("#muc").val()
+        // them 
+        if (option == 1) {
+            // console.log(option)
+            mucThem = $("#them").val()
+            themBh = $("#them-bh").val()
+            if (them == "bai hat da thich") {
+                //append
+
+                //     $('#box').append(
+                //         $('<div/>')
+                //           .attr("id", "newDiv1")
+                //           .addClass("newDiv purple bloated")
+                //           .append("<span/>")
+                //             .text("hello world")
+                //       );
+                // }
+
+
+                $('#all-bh').append(
+                    $('<div/>')
+                        .attr("id", "div-bh")
+                        .addClass("d-flex")
+                        .append("<a/>")
+                        .text(themBh)
+                );
+            }
+
+
+
+
+            //sua
+            if (option == 2) {
+                // console.log(option)
+
+
+            }
+            //xoa
+            if (option == 3) {
+                // console.log(option)
+
+            }
+
+        })
 
 
 
--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
+    $("#tao-playlist").click(function () {
 
-
-
-
+        // $("#notifications").notify( {
+        //     message: {
+        //         text: 'Aw yeah, It works!'
+        //     }
+        // }).show()
+        alert("ban chua dang nhap tai khoan")
+    });
+    $("#bai-hat-da-thich").click(function () {
+        alert("ban chua dang nhap tai khoan")
+    });
 
 
 
